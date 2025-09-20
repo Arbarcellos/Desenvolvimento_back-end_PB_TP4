ec6db84a2662172f2417a9b34f4231a7c4c741b5
Arbarcellos <arthurbarcellos@gmail.com>
2025-09-20 12:13:46 -0300

    Última atualização - AT

--- src/main/java/br/edu/infnet/arthurbarcellos_PB/ArthurbarcellosPbApplication.java
+++ src/main/java/br/edu/infnet/arthurbarcellos_PB/ArthurbarcellosPbApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ArthurbarcellosPbApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ArthurbarcellosPbApplication.class, args);
+        SpringApplication.run(ArthurbarcellosPbApplication.class, args);
+
 	}
 
 }
--- src/main/java/br/edu/infnet/arthurbarcellos_PB/model/domain/Main.java
+++ src/main/java/br/edu/infnet/arthurbarcellos_PB/model/domain/Main.java
@@ -1,202 +1,188 @@
 package br.edu.infnet.arthurbarcellos_PB.model.domain;
 
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
-
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class Main{
-	
-	static Scanner leitura = new Scanner (System.in);
-	static String caminhoArquivo = "clientes.csv";
-	
-	public static void main(String args[]) {
-		
-		gerenciador(obterEscolha(menu()));
-		
-	}
-	
-	public static int menu() {
-		//método responsável pelo menu do programa
-		System.out.println("\n---------------Lojas Amazones---------------");
-		System.out.println(" [1] - Login");
-		System.out.println(" [2] - Cadastrar conta");
-		System.out.println("\n [0] - Sair");
-		System.out.println("----------------------------------------\n");
-		
-		return 3;
-	}
-	public static int obterEscolha(int opcoes) {
-		//método responsável pela obtenção da escolha do menu feita pelo usuário. Nele é feita uma validação se a escolha é uma opção válida e retorna o integer escolhido.
-		//inicialização de variável integer
-		int escolha;
-		System.out.print(">>>> Digite o número o número da opção desejada: ");
-		escolha = Integer.valueOf(leitura.nextLine());
-		
-		//validação da escolha inputada pelo usuário. Caso não seja uma opção válida, repete
-		if(escolha >opcoes && escolha <0 ) {
-			System.out.println("Opção inválida! Digite um número de 0 a "+ opcoes + ".");
-			return obterEscolha(opcoes);
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-		}else {
-			return escolha;
-		}
-	}
-	public static void gerenciador(int escolha) {
-		//método para direcionamento da escolha feita pelo usuário. Recebe como argumento uma integer que é direcionado pelo SWITCH, em caso de uma opção inválida, sinaliza o usuário.
-		switch(escolha) {
-			case 1: login(); break;
-			case 2: cadastro(); break;
-			case 0: System.out.println("Muito obrigado! Aplicação finalizada."); break;
-			default:
-				System.out.println("Opção inválida! Digite um número de 0 a 2.");
-				gerenciador(obterEscolha(menu()));
-		}
-	}
-	
-	public static void login(){
-		// coletar email e senha
-		// abrir csv e buscar informação
-		// validar
-		// 		positivo: abrir tela de produtos
-		// 		negativo: solicitar email e senha novamente ou cadastrar conta
-		System.out.println("\n---------------LOGIN---------------");		
-		
-		System.out.print("Digite o seu e-mail: ");
-		String email = leitura.nextLine();
-		System.out.print("Digite sua senha: ");
-		String senha = leitura.nextLine();		
+import br.edu.infnet.arthurbarcellos_PB.model.dto.UsuarioDTO;
+import br.edu.infnet.arthurbarcellos_PB.model.service.UsuarioService;
 
+@Component
+public class Main {
+    static Scanner leitura = new Scanner(System.in);
+    static String caminhoArquivo = "clientes.csv";
 
-		if(validaEmailSenhaLeitor(email, senha)){
-			System.out.println("\nLogin realizado com sucesso!");
-			produtos();
-		} else {
-			System.out.println("\nEmail ou senha inválidos! Digite novamente.");
-			login();
-		}
-	}
-		
-	public static void produtos() {
-		//método responsável pelo menu do programa
-		System.out.println("\n---------------Produtos---------------");
-		System.out.println(" !!Página em desenvolvimento!!");
-		System.out.println("----------------------------------------\n");
+    @Autowired
+    private UsuarioService usuarioService;
 
-	}
-	public static void cadastro(){
-		// solicitar nome e email
-		// validar se email já existe
-		// 		negativo: 	solicitar senha
-		//					validar senha
-		//					mensagem de cadastro concluído e ir para tela de produtos
-		//		positivo:	informar que já existe.
-		System.out.println("\n---------------Cadastrar Conta---------------");		
-		
-		String nome;
-		
-		do{
-			System.out.print("Digite o seu nome completo: ");
-			nome = leitura.nextLine();
-		}while(nome == null);
-		
-		String email = solicitarEmail();
-		
-		if(!verificaLeitorEmail(email)){
-			String senha = solicitarSenha();
-			
-			Usuario cliente = new Usuario();
-		 	cliente.setNome(nome);
-		 	cliente.setEmail(email);
-		 	cliente.setSenha(senha);
-		 	
-		 	escritor(cliente);
-		 	
-			System.out.println("\nLogin realizado com sucesso!");
-			produtos();
-			
-		}else{
-			System.out.println("Conta de e-mail já existente. Realize seu login.");
-			login();
-		}
-		
-		}	
-	public static String solicitarEmail(){
-		String email = "asdf";
-		do {
-			
-			System.out.print("Digite seu email: ");
+    public void runMainLogic() {
+        gerenciador(obterEscolha(menu()));
+    }
+
+    public int menu() {
+        System.out.println("\n---------------Lojas Amazones---------------");
+        System.out.println(" [1] - Login");
+        System.out.println(" [2] - Cadastrar conta");
+        System.out.println("\n [0] - Sair");
+        System.out.println("----------------------------------------\n");
+        return 3;
+    }
+
+    public int obterEscolha(int opcoes) {
+        int escolha;
+        System.out.print(">>>> Digite o número da opção desejada: ");
+        escolha = Integer.parseInt(leitura.nextLine());
+        if(escolha > opcoes || escolha < 0 ) {
+            System.out.println("Opção inválida! Digite um número de 0 a " + opcoes + ".");
+            return obterEscolha(opcoes);
+        }
+        return escolha;
+    }
+
+    public void gerenciador(int escolha) {
+        switch(escolha) {
+            case 1: login(); break;
+            case 2: cadastro(); break;
+            case 0: System.out.println("Muito obrigado! Aplicação finalizada."); break;
+            default:
+                System.out.println("Opção inválida! Digite um número de 0 a 2.");
+                gerenciador(obterEscolha(menu()));
+        }
+    }
+
+    public void login(){
+        System.out.println("\n---------------LOGIN---------------");
+
+        System.out.print("Digite o seu e-mail: ");
+        String email = leitura.nextLine();
+        System.out.print("Digite sua senha: ");
+        String senha = leitura.nextLine();
+
+        if(validaEmailSenhaLeitor(email, senha)){
+            System.out.println("\nLogin realizado com sucesso!");
+            produtos();
+        } else {
+            System.out.println("\nEmail ou senha inválidos! Digite novamente.");
+            login();
+        }
+    }
+
+    public void produtos() {
+        System.out.println("\n---------------Produtos---------------");
+        System.out.println(" !!Página em desenvolvimento!!");
+        System.out.println("----------------------------------------\n");
+    }
+
+    public void cadastro(){
+        System.out.println("\n---------------Cadastrar Conta---------------");
+
+        String nome;
+        do{
+            System.out.print("Digite o seu nome completo: ");
+            nome = leitura.nextLine();
+        }while(nome == null);
+
+        String email = solicitarEmail();
+
+        if(!verificaLeitorEmail(email)){
+            String senha = solicitarSenha();
+
+            UsuarioDTO cliente = new UsuarioDTO();
+         	cliente.setNome(nome);
+         	cliente.setEmail(email);
+         	cliente.setSenha(senha);
+
+         	escritor(cliente);
+
+            usuarioService.salvar(cliente);
+
+            System.out.println("\nLogin realizado com sucesso!");
+            produtos();
+
+        }else{
+            System.out.println("Conta de e-mail já existente. Realize seu login.");
+            login();
+        }
+    }
+
+    public String solicitarEmail(){
+        String email = "asdf";
+        do {
+            System.out.print("Digite seu email: ");
         	email = leitura.nextLine();
-        	
+
         	if(!validaEmailRegex(email)){
 				System.out.println("Formato inválido de e-mail!");
 			}
-        	
+
        } while (!validaEmailRegex(email));
        return email;
-	}	
-	
-	
-	public static String solicitarSenha(){
-		String senha = "asdf";
-		do {
-			System.out.print("Cadastre uma senha. Ela deve conter no mínimo 8 caracteres, pelo menos uma letra maiúscula, um número e um caractere especial (@, #, $, etc.): ");
+    }
+
+    public String solicitarSenha(){
+        String senha = "asdf";
+        do {
+            System.out.print("Cadastre uma senha. Ela deve conter no mínimo 8 caracteres, pelo menos uma letra maiúscula, um número e um caractere especial (@, #, $, etc.): ");
         	senha = leitura.nextLine();
        } while (!validaSenha(senha));
        return senha;
-	}
-	
-	public static boolean verificaLeitorEmail(String email) {	
+    }
+
+    public boolean verificaLeitorEmail(String email) {
 		try {
 			FileReader arquivo = new FileReader(caminhoArquivo);
 			BufferedReader leitura = new BufferedReader(arquivo);
-	
-			String linha;	
+
+			String linha;
 			
 			while((linha = leitura.readLine()) != null) {
 				if (linha.contains(email)){	
 					leitura.close();		
 					return true;
-				};
+				}
 			}
 			leitura.close();
 		} catch (FileNotFoundException e) {
 			System.out.println("FileNotFound" + e.getMessage());
-	
+
 		} catch (IOException e) {
 			System.out.println("IO" + e.getMessage());
 		}
 		return false;	
 	}
-	public static boolean validaEmailSenhaLeitor(String email, String senha) {	
+
+    public boolean validaEmailSenhaLeitor(String email, String senha) {
 		try {
 			FileReader arquivo = new FileReader(caminhoArquivo);
 			BufferedReader leitura = new BufferedReader(arquivo);
-	
-			String linha;	
+
+			String linha;
 			
 			while((linha = leitura.readLine()) != null) {
 				if (linha.contains(email) & linha.contains(senha)){	
 					leitura.close();		
 					return true;
-				};
+				}
 			}
 			leitura.close();
 		} catch (FileNotFoundException e) {
 			System.out.println("FileNotFound" + e.getMessage());
-	
+
 		} catch (IOException e) {
 			System.out.println("IO" + e.getMessage());
 		}
 		return false;	
 	}
-	
-	public static void escritor(Usuario cliente) {
+
+    public void escritor(UsuarioDTO cliente) {
 		try {
 			String nome = cliente.getNome();
 			String email = cliente.getEmail();
@@ -205,15 +191,19 @@ public class Main{
 			FileWriter writer = new FileWriter(caminhoArquivo, true);
 			writer.write(nome + ";" + email + ";" + senha + "\n");
 			writer.close();
+
+            usuarioService.salvar(cliente);
+
 	        System.out.println("\nCliente "+ nome + " cadastrado com sucesso " + caminhoArquivo);
 		} catch (FileNotFoundException e) {
 			System.out.println("FileNotFound" + e.getMessage());
-	
+
 		} catch (IOException e) {
 			System.out.println("IO" + e.getMessage());
 		}		
 	}
-	public static boolean validaEmailRegex(String email) {
+
+    public boolean validaEmailRegex(String email) {
 		String regex = "(^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$)";
 		
 		Pattern pattern = Pattern.compile(regex);
@@ -221,8 +211,8 @@ public class Main{
 		
 		return matcher.matches();
 	}
-	
-	public static boolean validaSenha(String senha) {
+
+    public boolean validaSenha(String senha) {
 		String [] caracteresEspeciais = {"@", "%", "#", "!", "\\", "&", "*", "(", ")", "-", "+", "=", "[", "]", "{", "}", ";", ":", ",", ".", "<", ">", "/", "?", "|", "$", "~", "`", "'", "\""};
 		
 		boolean senhaValida = true;
@@ -267,6 +257,5 @@ public class Main{
 				
 		return senhaValida ;
 	}
-	
-	
 }
+
